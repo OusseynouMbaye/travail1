0d276a6edaf837a38cb5334de31667fed2364473
Ousseynou Mbaye <ouzinem@hotmail.com>
2020-02-07 10:38:26 -0500

    nouvelle modification dans donnees.cpp

--- travail1/donnees.cpp
+++ travail1/donnees.cpp
@@ -4,29 +4,31 @@
 Donnees::Donnees()
 {
 	
+
 }
 
 void Donnees::creerLePaquetDeCarte()
 {
-	int cpt=0;
-	//il faudrait aller creer les cartes de piques
-	lePaquetDeCarte[0].InitialiserCarte(1, "pique");
+	int inNumeroDesCartes=0;
 	
-	//il faudrait aller creer les cartes de coeur
-	//il faudrait aller creer les cartes de trefle
-	//il faudrait aller creer les cartes de carreau
+	creerLesCartes("pique", inNumeroDesCartes);
+	creerLesCartes("Coeur", inNumeroDesCartes);
+	creerLesCartes("Trefle", inNumeroDesCartes);
+	creerLesCartes("Carreau", inNumeroDesCartes);
+	
+
+
 
 }
-void Donnees::creerLesCartes(string inAtoutCarte, int &inCompteur)
+void Donnees::creerLesCartes(string inAtoutCarte, int &inNumeroDesCartes)
 {
 	
 	int ValeurCarte;
 	for (ValeurCarte = 1; ValeurCarte <=13 ; ValeurCarte++)
 	{
-
-		lePaquetDeCarte[inCompteur] = new Carte;
 		
-		lePaquetDeCarte[inCompteur].InitialiserCarte(ValeurCarte)
+		lePaquetDeCarte[inNumeroDesCartes].InitialiserCarte(ValeurCarte,inAtoutCarte);
+		inNumeroDesCartes++;
 	}
 
 }
